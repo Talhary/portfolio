108d6bd24d57415d2e29ea18064fa0e51323f0eb
Talhary <talhariaz5425869@gmail.com>
2024-06-14 18:30:41 +0500

    sdffs

--- sections/about.tsx
+++ sections/about.tsx
@@ -4,7 +4,6 @@ export const About = () => {
     <div className="text-black text-2xl dark:bg-dark-bg dark:text-white flex items-center  justify-center">
       <div className="flex flex-row max-md:flex-col justify-center items-center w-[100vw] h-screen ">
         <div className="w-[50%] max-md:w-[80%] flex items-center max-md:items-start max-md:mb-10  flex-col">
-           
           <div className="max-sm:hidden">
             <img
               src={"/image.png"}
@@ -13,12 +12,14 @@ export const About = () => {
               className="rounded-full"
             />
           </div>
-         <div >
-         <h2 className="text-4xl font-semibold mt-5 max-sm:text-2xl">Muhammad Talha</h2>
-          <p className="mt-2 text-2xl max-sm:text-xl">Web Developer</p>
-         </div>
+          <div className="flex flex-col items-start justify-start ">
+            <h2 className="text-4xl font-semibold mt-5 max-sm:text-2xl">
+              Muhammad Talha
+            </h2>
+            <p className="mt-2 text-2xl max-sm:text-xl">Web Developer</p>
+          </div>
         </div>
-        <div className="w-[45%] max-sm:px-4 max-sm:w-auto text-3xl max-md:text-2xl max-md:w-[80%] max-sm:text-xl">
+        <div className=" max-sm:w-auto text-3xl max-sm:px-10 max-md:text-2xl max-md:w-[80%] max-sm:text-xl w-[50%] max-md:w-[80%] flex items-center max-md:items-start max-md:mb-10  flex-col">
           <p>
             I'm a passionate web developer with a love for creating stunning and
             performant user interfaces. My journey in web development began with
@@ -30,9 +31,7 @@ export const About = () => {
           </p>
         </div>
       </div>
-      <div className="hidden">
-        TODO: ADD SOCIAL MEDIA LOGOS
-      </div>
+      <div className="hidden">TODO: ADD SOCIAL MEDIA LOGOS</div>
     </div>
   );
 };
