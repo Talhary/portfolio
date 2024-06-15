b2d941fcf7d15b9d5e81c18de4c2e47bb65dbbe5
Talhary <talhariaz5425869@gmail.com>
2024-06-15 14:44:03 +0500

    Updated Projects

--- sections/projects.tsx
+++ sections/projects.tsx
@@ -3,25 +3,18 @@ import Link from "next/link";
 const projcts = [
   {
     name:'NEXT Authentication APP',
-    weblink: "sfdds",
-    githublink: "http://www.google.com",
+    weblink: "https://next-auth-v5-by-talhary.vercel.app/",
+    githublink: "https://github.com/Talhary/NextJs-Authentication-App",
     imglink:'/next-auth.png' ,
-    stack: ["nodejs", "javascript", "express", "react", "nextjs"],
+    stack: ["Next.js",'TailwindCss','Typescript.js','NextAuth.js','Prisma'],
   },
   {
-    name:'NEXT Authentication APP',
-    weblink: "sfdds",
-    githublink: "http://www.google.com",
-    imglink:'/next-auth.png' ,
-    stack: ["nodejs", "javascript", "express", "react", "nextjs"],
-  },
-  {
-    name:'NEXT Authentication APP',
-    weblink: "sfdds",
-    githublink: "http://www.google.com",
-    imglink:'/next-auth.png' ,
-    stack: ["nodejs", "javascript", "express", "react", "nextjs"],
-  },
+    name:'Personal Gallery App',
+    weblink: "https://t3gallay.vercel.app/",
+    githublink: "https://github.com/Talhary/t3gallay",
+    imglink:'/gallary-app.png' ,
+    stack: ["Next.js", "Drizzle", "TypeScript", "TailwindCss", "Clerk.io",'Sentry'],
+  }
 ];
 export const Projects = () => {
   return (
@@ -33,15 +26,15 @@ export const Projects = () => {
       <div className="flex gap-x-10 mx-10 overflow-scroll">
         {projcts.map((project, i) => {
           return (
-            <div key={i + 1} className="max-w-[250px] dark:bg-card-bg   bg-primary1-light-hover rounded-t-[10px]">
-              <div className=" ">
-                <img className="rounded-t-[20px]" src={project.imglink} height={300} width={250} alt={project.name}/>
+            <div key={i + 1} className="max-w-[250px] dark:bg-card-bg   bg-primary1-light-hover rounded-xl">
+              <div className=" p-2">
+                <img className="rounded-[20px]" src={project.imglink} height={300} width={250} alt={project.name}/>
               </div>
-              <h2 className="dark:text-white text-black mt-2 mx-3 font-semibold">{project.name}</h2>
+              <h2 className=" dark:text-white text-black mt-4 mx-3 font-semibold">{project.name}</h2>
               <div className="flex flex-wrap mx-2 mt-5 gap-y-1">
                 {project.stack.map((el, i) => {
                   return (
-                    <div className="dark:bg-dark-bg dark:text-white bg-white border border-primary1-normal rounded-full odd:border-tertiary-normal text-primary1-darker  px-2 py-1 m-1" key={i + 1}>
+                    <div className="text-sm dark:bg-dark-bg dark:text-white bg-white border border-primary1-normal rounded-full odd:border-tertiary-normal text-primary1-darker  px-2 py-1 m-1" key={i + 1}>
                       <span >{el}</span>
                     </div>
                   );
